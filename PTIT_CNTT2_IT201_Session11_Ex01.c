2395efcb160ea2824e59d82038ac8b229db06a20
peyin192 <vuonglinh190206.qv@gmail.com>
2025-07-06 21:29:37 +0700

    PTIT_CNTT2_IT201_Session11_Ex01.c

--- PTIT_CNTT2_IT201_Session11_Ex01.c
+++ PTIT_CNTT2_IT201_Session11_Ex01.c
@@ -1,61 +1,58 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Node{
-     int data;
-     struct Node* next;
-     struct Node* prev;
-}Node;
-Node*createNode(int value){
-     Node*newNode=(Node*)malloc(sizeof(Node));
-     newNode->data=value;//gán giá trị cho node
-     newNode->next=NULL;//gán giá trị cho con trỏ next
-     newNode->prev=NULL;//in giá trị cho con trỏ prev
-     return newNode;
-}
-//hàm in danh sách liên kết đôi
-void printList(Node* head){
-     int index=1;
-     Node* temp=head;
-     printf("Danh sach lien ket doi: \n");
-     while(temp!=NULL){
-         printf("Node %d :%d ",index++,temp->data);
-         temp=temp->next;
-         printf("\n");
-     }
+typedef struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
 
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
 }
-//hàm giải phóng bộ nhớ
-void freeList(Node* head){
-    Node* temp=head;
-    while(head!=NULL){
-       temp=head;
-       head=head->next;//tr ến con trỏ tiếp theo
-       free(temp);//giải phóng các node
+
+void printList(Node* head) {
+    int index = 1;
+    Node* temp = head;
+    printf("Danh sach lien ket doi: \n");
+    while (temp != NULL) {
+        printf("Node %d :%d ", index++, temp->data);
+        temp = temp->next;
+        printf("\n");
     }
-
 }
-int main(){
 
-    Node*node1=createNode(10);
-    Node*node2=createNode(20);
-    Node*node3=createNode(30);
-    Node*node4=createNode(40);
-    Node*node5=createNode(50);
+void freeList(Node* head) {
+    Node* temp = head;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
 
-    //Gán các node với nhau
-    node1->next=node2;
-    node2->prev=node1;
-    node2->next=node3;
-    node3->prev=node2;
-    node3->next=node4;
-    node4->prev=node3;
-    node4->next=node5;
-    node5->prev=node4;
+int main() {
+    Node* node1 = createNode(10);
+    Node* node2 = createNode(20);
+    Node* node3 = createNode(30);
+    Node* node4 = createNode(40);
+    Node* node5 = createNode(50);
 
-    Node* head=node1;
+    node1->next = node2;
+    node2->prev = node1;
+    node2->next = node3;
+    node3->prev = node2;
+    node3->next = node4;
+    node4->prev = node3;
+    node4->next = node5;
+    node5->prev = node4;
+
+    Node* head = node1;
     printList(head);
-    //giải phóng ộ nhớ
     freeList(head);
     return 0;
 }
